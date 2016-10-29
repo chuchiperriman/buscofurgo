90a62ad00dfd082f121069d87a3a7c31b630e806
Jesús Barbero <chuchiperriman@gmail.com>
2016-10-29 10:23:28 +0200

    Organizamos un poco mejor el código de m.a.

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# Pasos siguientes
+
+* Obtener los datos de todas las páginas de la búsqueda
+* Guardar la búsqueda para poder trabajar offline
+* Mostrar datos en tabla con columnas ordenables
+* Exportar datos en Excel (o tabla para poder pegar en Excel)
+
+# TODOs
+
+
+# Saco de ideas
--- routes/search.js
+++ routes/search.js
@@ -8,70 +8,103 @@ var cheerio = require('cheerio');
 var S = require('string');
 var app = express();
 
-router.get('/', function(req, res) {
+var getX11Prop = function($el, prop){
+    return $el.find("." + prop).contents().first().text();
+};
 
-    var getX11Prop = function($el, prop){
-        return $el.find("." + prop).contents().first().text();
-    };
+var getMaData = function(html){
 
-    url = 'http://www.milanuncios.com/anuncios-en-cantabria/t5-multivan.htm?desde=3000';
+    var $ = cheerio.load(iconv.decode(html, 'iso-8859-15').toString('utf-8'));
+
+    var json = {
+        adds: [],
+        pages: []
+    }
+    var adds = [];
+
+    $("div.aditem").each(function(i, elem) {
+        var data = $(this);
+        var props = data.find(".x11 .inmo-attributes");
+        var header = data.find(".aditem-header").text();
+        var $body = data.find(".aditem-detail-image-container, .aditem-detail");
+        if (!$body.length){
+            $body = data.find(".x9");
+        }
+        var $title = $body.find("a.aditem-detail-title");
+        var text = $body.find(".tx").text();
+
+        json.adds.push({
+            title: $title.text(),
+            link: "http://www.milanuncios.com" + $title.attr('href'),
+            description: text,
+            price: $body.find('.aditem-price').text(),
+            year: getX11Prop(props, "ano"),
+            mileage: getX11Prop(props, "kms"),
+            cv: getX11Prop(props, "cc"),
+            professional: data.find(".vem.pro").length > 0,
+            automatic: data.find(".cauto").length > 0,
+            province: S(header).between('(', ')').s.trim() ,
+            location: S(header).contains(' en ') ? S(header).between(' en ', '(').s : ''
+        });
+
+    });
+
+    $('div.adlist-paginator-pages .adlist-paginator-pagelink').each(function(i, elem){
+        var data = $(this);
+        if (!data.hasClass('adlist-paginator-pageselected')){
+            json.pages.push({
+                number: parseInt(data.find('a').text()),
+                queryString: data.find('a').attr('href')
+            });
+        }
+    });
+    return json;
+};
+
+var requestMaData = function(url, queryString, json, rescallback){
+
+    console.log("Entramos");
 
     request({
-        uri: url,
+        uri: url + queryString,
         encoding: null
     }, function(error, response, html) {
         if (!error) {
-            var $ = cheerio.load(iconv.decode(html, 'iso-8859-15').toString('utf-8'));
-            var json = {
-                results: []
-            };
-            $("div.aditem").each(function(i, elem) {
-                var data = $(this);
-                var props = data.find(".x11 .inmo-attributes");
-                var header = data.find(".aditem-header").text();
-                var $body = data.find(".aditem-detail-image-container, .aditem-detail");
-                if (!$body.length){
-                    $body = data.find(".x9");
+            var madata = getMaData(html);
+            if (madata.adds){
+                json.results = json.results.concat(madata.adds);
+                console.log("Concat");
+            }
+            madata.pages.forEach(function(page){
+                console.log('Procesando ' + page.number);
+                if (json.pages.indexOf(page.number) > -1){
+                    json.pages.push(page.number);
+                    requestMaData(url, page.queryString, json, undefined);
+                    console.log("Procesada " + page.number);
                 }
-                var $title = $body.find("a.aditem-detail-title");
-                var text = $body.find(".tx").text();
-                //console.log(text);
-                json.results.push({
-                    title: $title.text(),
-                    link: "http://www.milanuncios.com" + $title.attr('href'),
-                    description: text,
-                    price: $body.find('.aditem-price').text(),
-                    year: getX11Prop(props, "ano"),
-                    mileage: getX11Prop(props, "kms"),
-                    cv: getX11Prop(props, "cc"),
-                    professional: data.find(".vem.pro").length > 0,
-                    automatic: data.find(".cauto").length > 0,
-                    province: S(header).between('(', ')').s.trim() ,
-                    location: S(header).contains(' en ') ? S(header).between(' en ', '(').s : ''
-                });
             });
         }
+        if (rescallback){
+            console.log("callback");
+            rescallback(json);
+        }
+        console.log("Terminamos");
+    });
+    console.log("Salimos");
+};
 
-        // To write to the system we will use the built in 'fs' library.
-        // In this example we will pass 3 parameters to the writeFile function
-        // Parameter 1 :  output.json - this is what the created filename will be called
-        // Parameter 2 :  JSON.stringify(json, null, 4) - the data to write, here we do an extra step by calling JSON.stringify to make our JSON easier to read
-        // Parameter 3 :  callback function - a callback function to let us know the status of our function
+router.get('/', function(req, res) {
 
-        /*
-        fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err) {
+    var json = {
+        results: [],
+        //La Primera página la damos por procesada siempre
+        pages: [1]
+    };
 
-            console.log('File successfully written! - Check your project directory for the output.json file');
-
-        })
-        */
-        //console.log(JSON.stringify(json, null, 4));
-
-        //res.send('Check your console! <br/>' + JSON.stringify(json, null, 4));
-        res.json(json);
-
-    })
-
+    requestMaData('http://www.milanuncios.com/anuncios-en-cantabria/t5-multivan.htm',
+        '?desde=3000&demanda=n&orden=baratos&cerca=s', json, function(){
+            res.json(json);
+        });
 });
 
 module.exports = router;
